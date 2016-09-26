db8400f816e36e65fd4abbc4a241e0565f41695b
borysn <xborysn@gmail.com>
2016-09-26 13:30:43 -0500

    test generateRandomMove

--- src/test/java/tictactoe/game/player/TicTacToeCpuStrategyTest.java
+++ src/test/java/tictactoe/game/player/TicTacToeCpuStrategyTest.java
@@ -2,9 +2,14 @@ package tictactoe.game.player;
 
 import org.junit.Assert;
 import org.junit.Test;
+import tictactoe.game.board.TicTacToePoint;
+import tictactoe.game.engine.Move;
 import tictactoe.game.engine.TicTacToeMove;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
@@ -20,6 +25,29 @@ public class TicTacToeCpuStrategyTest {
         Assert.assertEquals("Should generate move 3,3",
                 new TicTacToeMove(3,3),
                 strategyMock.generateMove(new ArrayList<>()));
+    }
+
+    @Test
+    public void testGenerateRandomMove() {
+        // init empty points
+        Set<TicTacToePoint> emptyPoints = new LinkedHashSet<>();
+        emptyPoints.add(new TicTacToePoint(0,0));
+        emptyPoints.add(new TicTacToePoint(0,1));
+        emptyPoints.add(new TicTacToePoint(0,2));
+        emptyPoints.add(new TicTacToePoint(1,0));
+        emptyPoints.add(new TicTacToePoint(1,1));
+        emptyPoints.add(new TicTacToePoint(1,2));
+        emptyPoints.add(new TicTacToePoint(2,0));
+        emptyPoints.add(new TicTacToePoint(2,1));
+        emptyPoints.add(new TicTacToePoint(2,2));
+
+        // generate move
+        Move move = strategyMock.generateRandomMove(emptyPoints.stream().collect(Collectors.toList()));
+        // to tictactoe gameboard point
+        TicTacToePoint point = new TicTacToePoint(move.getX()-1, move.getY()-1);
+
+        // move should be contained in empty points set
+        Assert.assertTrue("Generated move exists in empty points set", emptyPoints.contains(point));
     }
 
 }
