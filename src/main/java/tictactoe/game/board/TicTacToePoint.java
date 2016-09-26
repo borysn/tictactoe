fe0736b2d83b369612b52e601cc0abbb15f86d26
borysn <xborysn@gmail.com>
2016-09-26 13:30:26 -0500

    implement equals and hashCode

--- src/main/java/tictactoe/game/board/TicTacToePoint.java
+++ src/main/java/tictactoe/game/board/TicTacToePoint.java
@@ -1,5 +1,7 @@
 package tictactoe.game.board;
 
+import java.util.Objects;
+
 public class TicTacToePoint implements BoardPoint {
     private String value;
     private int xPos;
@@ -8,6 +10,29 @@ public class TicTacToePoint implements BoardPoint {
     public TicTacToePoint(int xPos, int yPos) {
         this.value = TicTacToePointValue.EMPTY.value();
         this.setPosition(xPos, yPos);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + this.xPos;
+        result = 31 * result + this.yPos;
+        result = 31 * result + this.value.hashCode();
+        return 31 * result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o != null && o instanceof TicTacToePoint) {
+            TicTacToePoint point = (TicTacToePoint) o;
+            return this.xPos == point.getXPos() && this.yPos == point.getYPos();
+        }
+
+        return false;
     }
 
     @Override
