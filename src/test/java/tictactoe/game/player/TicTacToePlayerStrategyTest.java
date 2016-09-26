bc6a71050ead933907b988937230914340a7d192
borysn <xborysn@gmail.com>
2016-09-26 11:28:28 -0500

    test parse move

--- src/test/java/tictactoe/game/player/TicTacToePlayerStrategyTest.java
+++ src/test/java/tictactoe/game/player/TicTacToePlayerStrategyTest.java
@@ -11,17 +11,43 @@ import static org.mockito.Mockito.when;
 
 public class TicTacToePlayerStrategyTest {
 
-    TicTacToePlayerStrategy strategy = mock(TicTacToePlayerStrategy.class);
+    TicTacToePlayerStrategy strategyMock = mock(TicTacToePlayerStrategy.class);
 
     @Test
     public void testGenerateMove() {
         // define playerMove return
-        when(strategy.generateMove(new ArrayList<>()))
+        when(strategyMock.generateMove(new ArrayList<>()))
                 .thenReturn(new TicTacToeMove(3,3));
 
         Assert.assertEquals("Player strategy generate move",
-                strategy.generateMove(new ArrayList<>()),
+                strategyMock.generateMove(new ArrayList<>()),
                 new TicTacToeMove(3,3));
+    }
+
+    @Test
+    public void testParseMove() {
+        TicTacToePlayerStrategy strategy = new TicTacToePlayerStrategy();
+
+        String badMove1 = "3.3";
+        String badMove2 = "33";
+        String badMove3 = "asdf";
+        String correctMove = "3,3";
+
+        Assert.assertEquals("badMove1 should return invalid move",
+                new TicTacToeMove(0,0),
+                strategy.parseMove(badMove1));
+
+        Assert.assertEquals("badMove2 should return invalid move",
+                new TicTacToeMove(0,0),
+                strategy.parseMove(badMove2));
+
+        Assert.assertEquals("badMove3 should return invalid move",
+                new TicTacToeMove(0,0),
+                strategy.parseMove(badMove3));
+
+        Assert.assertEquals("correctMove should return a valid move",
+                new TicTacToeMove(3,3),
+                strategy.parseMove(correctMove));
     }
 
 }
